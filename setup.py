322f4280a3cd57ba9957c96d37f28ec35da25776
kiraDarthur <arthurandrade8713@gmail.com>
2024-01-10 20:09:47 +0000

    Set up project initial organization and metadata
    -Add setup.py to turn installable
    -Add__main__for executable
    -Add__init__for package
    
    closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name ="dundie",
+    version="0.1.0",
+    description= "Reward Point System for Dunder Mifflin",
+    author="Arthur Fonseca",
+    packages=find_packages(),
+)
